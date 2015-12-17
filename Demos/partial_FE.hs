f02b9c34184a007437cd303e850348ec7ab8dbaa
Maximilian Algehed <m.algehed@gmail.com>
2015-12-17 20:57:41 +0100

    partial FE cleanup

--- Demos/partial_FE.hs
+++ Demos/partial_FE.hs
@@ -2,16 +2,6 @@
 import Test.QuickCheck
 import QuickSpec
 
-data Greater = G {x::Int , y::Int} deriving (Eq, Ord, Typeable)
-          
-instance Arbitrary Greater where
-    arbitrary =
-        do
-            x <- arbitrary
-            y <- arbitrary `suchThat` (x>)
-            return (G x y)
-    shrink (G a b) = [G a' b' | b' <- [b, b-1..], a' <- [a, a-1..], a' > b'] 
-
 partial :: Int -> Int -> Int
 partial x y 
     | x >= y = x
