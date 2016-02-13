e86cda9f4d82d49712e21ee7f36fad4d72c07cc4
Maximilian Algehed <m.algehed@gmail.com>
2016-02-13 22:01:30 +0100

    Fixing Rohable

--- TeExamples/Combinators/APL.hs
+++ TeExamples/Combinators/APL.hs
@@ -1,27 +1,49 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
+
+module APL where
+
 import Data.Vector as V
 
-class (Eq a) => APLValue a
+class APLValue a where
+    
+    apl_eq :: a -> a -> Bool
+
+instance APLValue Char where
+    apl_eq = (==)
+
+instance APLValue Int where
+    apl_eq = (==)
 
 class Rohable a where
     
     roh_m :: a -> V.Vector Int
 
-instance APLValue Int
-instance APLValue Char
-
-instance (APLValue a) => Rohable a where
+instance Rohable Int where
     
     roh_m = const V.empty
 
-instance (APLValue a) => Rohable (V.Vector a) where
+instance Rohable Char where
     
-    roh_m = V.singleton . V.length
+    roh_m = const V.empty
 
-instance (APLValue a) => Rohable (V.Vector (V.Vector a)) where
+instance (Rohable a) => Rohable (V.Vector a) where
+    
+    roh_m v = (V.concatMap roh_m v) V.++ (V.singleton (V.length v))
 
-    roh_m = V.map V.length
+class Tildeable a where
+    
+    tilde :: a -> a
+
+instance Tildeable Int where
+
+    tilde 0 = 1
+    tilde 1 = 0
+    tilde _ = error "Domain error"
+
+instance Tildeable a => Tildeable (V.Vector a) where
+
+    tilde = V.map tilde
 
 iota_m :: Int -> V.Vector Int
 iota_m = V.enumFromN 1
@@ -30,7 +52,7 @@ iota_d :: (APLValue a) => V.Vector a -> V.Vector a -> V.Vector Int
 v `iota_d` w = V.map (iota_index v) w
     where
         iota_index :: (APLValue a) => V.Vector a -> a -> Int
-        iota_index v a = case findIndex (a==) v of
+        iota_index v a = case findIndex (apl_eq a) v of
                             Just i  -> i+1
                             Nothing -> (V.length v)+1
 
